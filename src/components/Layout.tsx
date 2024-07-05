8238e0e3f5878c8c81d9dd4faaffdf02c3cec0b0
Sianglife <sianglifen@gmail.com>
2024-07-06 03:20:17 +0800

    add: a part of next-session

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import NavBar from "@/components/navbar";
-import { SessionProvider } from "next-auth/react";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
@@ -8,12 +7,10 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 
 const Layout = ({ children }: any) => { 
     return (
-        <SessionProvider>
-            <div>
-                <NavBar />
-                {children}
-            </div>
-        </SessionProvider>
+        <div>
+            <NavBar />
+            {children}
+        </div>
     );
 }
 
---
+++ src/lib/session.tsx
@@ -0,0 +1,23 @@
+import EnvVars from "@/constants/EnvVars";
+import nextSession from 'next-session';
+import { expressSession, promisifyStore } from "next-session/lib/compat";
+
+//session
+const mongoURI = EnvVars.DB.URI;
+const MongoDBStore = require('connect-mongodb-session')(expressSession);
+const store = new MongoDBStore({
+    uri: mongoURI,
+    collection: 'Sessions'
+});
+store.on('error', function (err: Error) {
+    console.log(err);
+});
+
+export const getSession = nextSession({
+    name: 'dsoj_session',
+    store: store,
+    cookie: {
+      maxAge: 1000 * 60 * 60 * 24 * 7,
+    },
+  }
+)
