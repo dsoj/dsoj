da1b727eabf14aff398f937dada27603da0c3f33
Sianglife <sianglifen@gmail.com>
2024-07-09 00:58:30 +0800

    add: EnvVars seesion part

--- src/constants/EnvVars.tsx
+++ src/constants/EnvVars.tsx
@@ -9,23 +9,6 @@ import { getMongoURI } from '@/utils/MongoUtils';
 export default {
   NodeEnv: (process.env.NODE_ENV ?? ''),
   Port: (process.env.PORT ?? 0),
-  CookieProps: {
-    Key: 'ExpressGeneratorTs',
-    Secret: (process.env.COOKIE_SECRET ?? ''),
-    // Casing to match express cookie options
-    Options: {
-      httpOnly: true,
-      signed: true,
-      path: (process.env.COOKIE_PATH ?? ''),
-      maxAge: Number(process.env.COOKIE_EXP ?? 0),
-      domain: (process.env.COOKIE_DOMAIN ?? ''),
-      secure: (process.env.SECURE_COOKIE === 'true'),
-    },
-  },
-  Jwt: {
-    Secret: (process.env.JWT_SECRET ??  ''),
-    Exp: (process.env.COOKIE_EXP ?? ''), // exp at the same time as the cookie
-  },
   DB: {
     host: (process.env.DB_HOST ?? ''),
     password: (process.env.DB_PASSWD ?? ''),
@@ -35,7 +18,9 @@ export default {
   judge0: {
     host: (process.env.JUDGE0_HOST ?? ''),
   },
-  judge: {
-    TasksPath: (process.env.PROBLEM_TASK_PATH ?? ''),
-  },
+  session: {
+    secret: (process.env.SESSION_SECRET ?? ''),
+    maxAge: (process.env.SESSION_MAX_AGE ?? ''),
+    updateAge: (process.env.SESSION_UPDATE_AGE ?? ''),
+  }
 } as const;
