d174ed0a8c07a86d6a2b2f628475043e6c198b7e
Sianglife <sianglifen@gmail.com>
2024-08-27 20:51:52 +0800

    add: `/tags/` problemList page

--- src/pages/tags/[...tagname].tsx
+++ src/pages/tags/[...tagname].tsx
@@ -4,27 +4,14 @@ import ErrorPage from 'next/error';
 
 import EnvVars from "@/constants/EnvVars";
 import { DifficultyElement, TagElement } from "@/components/list_element";
-import { IProblem, IProblemListItem } from "@/interface/IProblem";
-import AlertMessage from "@/components/alert";
-import { useState } from "react";
-import { Editor as CodeEditor } from "@monaco-editor/react";
-import apiUrl from "@/constants/apiUrl";
-import axios from "axios";
-import { useRouter } from "next/router";
-import { Button } from "react-bootstrap";
+import { IProblemListItem } from "@/interface/IProblem";
+import { Card, Container, Spinner, Table } from "react-bootstrap";
 import Link from "next/link";
 
-export default function TagProblemList({ problemList }: { problemList: IProblem }) {
+export default function TagProblemList({ tag_name, problemList }: { tag_name: string, problemList: IProblemListItem[] }) {
     if (!problemList) {
         return (
             <ErrorPage statusCode={404} />
-        )
-    }
-    function genTagElement(tag: string) {
-        return (
-            <Button className="btn btn-primary" type="button" href={`/tag/${tag}`} key={tag} style={{ height: "1.5rem", padding: 0, fontSize: "0.8rem", paddingLeft: "0.5rem", paddingRight: "0.5rem", marginRight: "0.3rem", background: "rgb(190,190,190)", borderStyle: "none", borderTopStyle: "none" }}>
-                #{tag}
-            </Button>
         )
     }
 
@@ -61,29 +48,28 @@ export default function TagProblemList({ problemList }: { problemList: IProblem 
                     <DifficultyElement difficulty={difficulty} />
                 </td>
                 <td>
-                    <span>{tags.map((item) => genTagElement(item))}</span>
+                    <span>{tags.map((item: string) => TagElement(item))}</span>
                 </td>
             </tr>
         )
     }
 
     function TableContent() {
-        let TableElements: React.JSX.Element[] = [];
-        for (let i = 0; i < problems.length; i++) {
-            TableElements.push(genTableElement(i, problems[i]));
-        }
+        let TableElements: React.JSX.Element[] =
+            problemList.map((problem, index) => genTableElement(index, problem));
 
-        return (
-            <tbody>
-                {TableElements.length != 0 ? (
-                    TableElements
-                ) : (
-                    <Spinner animation='border' variant='primary' role='status'>
-                        <span className='visually-hidden'>Loading...</span>
-                    </Spinner>
-                )}
-            </tbody>
-        );
+        if (TableElements.length == 0) {
+            return (
+                <tr>
+                    <td colSpan={5}>
+                        <Spinner animation='border' variant='primary' role='status'>
+                            <span className='visually-hidden'>Loading...</span>
+                        </Spinner>
+                    </td>
+                </tr>
+            );
+        }
+        return TableElements
     }
 
     return (
@@ -98,7 +84,7 @@ export default function TagProblemList({ problemList }: { problemList: IProblem 
                                 marginBottom: "1rem!important",
                             }}
                         >
-                            problems
+                            Tag: {tag_name}
                         </Card.Title>
                     </Card.Header>
 
@@ -114,6 +100,7 @@ export default function TagProblemList({ problemList }: { problemList: IProblem 
                                 width: "15rem",
                             }}
                             placeholder='Search titles or tags'
+                            hidden={EnvVars.CommingSoon}
                         />
                         <Table>
                             <thead>
@@ -154,12 +141,14 @@ export default function TagProblemList({ problemList }: { problemList: IProblem 
                                     </th>
                                 </tr>
                             </thead>
-                            <TableContent />
+                            <tbody>
+                                <TableContent />
+                            </tbody>
                         </Table>
                     </Card.Body>
                 </Card>
             </Container>
-        </Layout>
+        </Layout >
     );
 }
 
@@ -167,18 +156,19 @@ export async function getServerSideProps(context: any) {
     const mongoURI = EnvVars.DB.URI;
     const mongo = new MongoClient(mongoURI);
 
-    const tag = context.query.id[0];
+    const tag_name = context.query.tagname[0];
 
-    let problemList = (await mongo
+    let problemList = await mongo
         .db("Judge")
         .collection("Problems")
-        .find({ tags: tag }, { projection: { _id: 0 } }))
+        .find({ tags: tag_name }, { projection: { _id: 0 } })
         .toArray()
-        .catch((err)=>{
+        .catch((err) => {
             console.error(err);
         })
     return {
         props: {
+            tag_name: tag_name,
             problemList: problemList,
         },
     };
