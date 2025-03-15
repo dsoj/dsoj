1f3b0d179d92cf093b597b8a32d562dcab640bed
Siang Huang <sianglifen@gmail.com>
2025-03-15 18:11:23 +0800

    Add /about

---
+++ src/app/about/page.tsx
@@ -0,0 +1,41 @@
+import about_info from "@/constants/about_information";
+import Link from "next/link";
+import { Container, Row, Col, Button } from 'react-bootstrap';
+
+
+export default function About() {
+    const { card_data, github_data } = about_info;
+    return (
+        <Container className="mt-5">
+            <Row>
+                <Col>
+                    <h1>About Us</h1>
+                    <p>
+
+                    </p>
+                </Col>
+            </Row>
+            <Row className="mt-4">
+                {/* {generateAboutCard()} */}
+            </Row>
+            <Row className="mt-4">
+                <Col>
+                    <Button
+                        href={github_data.repo_link}
+                        target="_blank"
+                        variant="dark"
+                    >
+                        <i className="bi bi-github"></i>
+                        &nbsp;
+                        {github_data.repo_name}
+                    </Button>
+                </Col>
+                <Col>
+                    <Link className="link-secondary" href={github_data.project_link}>
+                        Wanna Contribute? View more on GitHub Project!
+                    </Link>
+                </Col>
+            </Row>
+        </Container>
+    );
+}
