be9b5ab4aa7c00757c3cf4d88ada1a3532c3ac04
Siang Huang <sianglifen@gmail.com>
2025-04-30 23:30:37 +0800

    Fix logout redirect

--- src/app/api/auth/logout/route.ts
+++ src/app/api/auth/logout/route.ts
@@ -1,4 +1,6 @@
 import { NextResponse } from 'next/server';
+import { redirect } from 'next/navigation';
+import envVars from '@/constants/EnvVars';
 
 export async function GET(req: Request) {
     const res = NextResponse.redirect(new URL('/login', req.url));
