bae6698372b9809d083123b0899d4dbdb1977615
Siang Huang <sianglifen@gmail.com>
2025-05-15 14:05:07 +0800

    Fix hook inside suspense

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import { Metadata } from 'next';
 import Login from './Login';
+import { Suspense } from 'react';
 export const metadata: Metadata = {
     title: 'Login',
     description: 'Begin your journey with DSOJ.',
@@ -7,6 +8,8 @@ export const metadata: Metadata = {
 
 export default function LoginPage() {
     return (
-        <Login />
+        <Suspense fallback={<div>Loading...</div>}>
+            <Login />
+        </Suspense>
     );
 }
