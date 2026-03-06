d4a044ecb2e69df53d55447e479fcf851313310d
sianglife <sianglifen@gmail.com>
2026-03-06 11:43:44 +0800

    fix: middleware to proxy

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from 'next/server';
 import Logger from './lib/Logger';
 
-export function middleware(req: NextRequest) {
+export function proxy(req: NextRequest) {
     const ip = req.headers.get('x-forwarded-for') ?? 'unknown';
     const ua = req.headers.get('user-agent') ?? 'unknown';
     const path = req.nextUrl.pathname;
